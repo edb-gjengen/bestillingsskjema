84a075de40a6379e26c7e95a40fb0d23e79e866d
Robert Kolner <robert.kolner@gmail.com>
2013-04-24 19:29:37 +0200

    Added Twitter Bootstrap.

--- bestilling/design/models.py
+++ bestilling/design/models.py
@@ -2,4 +2,16 @@ from django.db import models
 
 # Create your models here.
 class DesignOrder(models.Model):
+    client = models.CharField(max_length=50)
+    deadline = models.DateField()
+    contact_name = models.CharField(max_length=50)
+    contact_email = models.CharField(max_length=50)
+    contact_number = models.CharField(max_length=20)
+    format = models.CharField(max_length=50)
+    format_other = models.CharField(max_length=50)
+    paper_size = models.CharField(max_length=50)
+    paper_size_other = models.CharField(max_length=50)
+    colour = models.CharField(max_length=50)
+    marger = models.CharField(max_length=50)
+    content = models.CharField(max_length=50)
     created = models.DateTimeField(auto_now_add=True)
