874b3cc3d06fa702a3da9638b9b503f16471d8ac
Robert Kolner <robert.kolner@gmail.com>
2013-04-27 17:41:52 +0200

    Fixed a bug which caused incorrect data to be sent to Trello when choosing 'other' option.

--- bestilling/design/views.py
+++ bestilling/design/views.py
@@ -47,16 +47,20 @@ class DesignFormView(BaseFormView):
         return dict((error, True) for error, has_happened in errors.iteritems() if has_happened is True)
 
     def _save_data(self, params):
+        format_type = params['format'] if params['format'] != 'other' else params['format_other']
+        paper_size = params['paper_size'] if params['paper_size'] != 'other' else params['paper_size_other']
+
         description = u"""\
-Forening: {client}
-Navn: {contact_name}
-Epost: {contact_email}
-Tlf: {contact_number}
-Format: {format}
-Papirstr.: {paper_size}
-Farger: {colour}
-Marger: {marger}
-Beskrivelse: {content}""".format(**params)
+**Fra:** {client}
+**Navn:** {contact_name}
+**Epost:** {contact_email}
+**Tlf:** {contact_number}
+**Format:** {format_type}
+**Papirstr.:** {paper}
+**Farger:** {colour}
+**Marger:** {marger}
+---
+{content}""".format(format_type=format_type, paper=paper_size, **params)
         deadline = datetime.strptime(params['deadline'], '%Y-%m-%d')
 
         card_id = self._save_to_trello(
@@ -71,8 +75,8 @@ Beskrivelse: {content}""".format(**params)
                 contact_name = params['contact_name'],
                 contact_email = params['contact_email'],
                 contact_number = params['contact_number'],
-                format_type = params['format'] if params['format'] != 'other' else params['format_other'],
-                paper_size = params['paper_size'] if params['paper_size'] != 'other' else params['paper_size_other'],
+                format_type = format_type,
+                paper_size = paper_size,
                 colour = params['colour'],
                 marger = params['marger'],
                 content = params['content'],
