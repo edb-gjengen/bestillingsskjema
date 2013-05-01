870821e56eb5aaa0da13b487284b05408269724d
Robert Kolner <robert.kolner@gmail.com>
2013-05-01 13:04:48 +0200

    Extracted part of models to a common file.

--- bestilling/design/models.py
+++ bestilling/design/models.py
@@ -1,15 +1,10 @@
+from bestilling.models import Order
 from django.db import models
 
-class DesignOrder(models.Model):
-    client = models.CharField(max_length=50)
-    deadline = models.DateField()
-    contact_name = models.CharField(max_length=50)
-    contact_email = models.CharField(max_length=50)
-    contact_number = models.CharField(max_length=20, blank=True, null=True)
+class DesignOrder(Order):
     format_type = models.CharField(max_length=50)
     paper_size = models.CharField(max_length=50)
     colour = models.CharField(max_length=50)
     marger = models.CharField(max_length=50)
     content = models.TextField(blank=True, null=True)
-    created = models.DateTimeField(auto_now_add=True)
-    trello_card_id = models.CharField(max_length=40)
+
