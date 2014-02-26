53b6470257f46e7c42136964eeb702c5c39f4d2c
Robert Kolner <robert.kolner@gmail.com>
2014-02-26 20:40:55 +0100

    I am stupid.

--- bestilling/prm/models.py
+++ bestilling/prm/models.py
@@ -2,6 +2,6 @@ from bestilling.models import Order
 from django.db import models
 
 class PrmOrder(Order):
-    assignment_type = models.CharField(max_length=50)
+    assignment_type = models.CharField(max_length=200)
     content = models.TextField(blank=True, null=True)
 
