0f6fef809fe46b0c1b865fff08796b940f3d7246
Nikolai Kristiansen <nikolaik@gmail.com>
2015-04-07 14:10:45 +0200

    Minor

--- bestilling/settings.py
+++ bestilling/settings.py
@@ -89,7 +89,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = ''
+SECRET_KEY = 'asdf123'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
